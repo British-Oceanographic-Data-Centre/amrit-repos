19a71093849ef5bdd730347175bba884273853a0
Yvanohe <yvanohe@gmail.com>
2025-06-05 11:30:45 +0200

    handle alert custom params request filters

---
+++ gateway/src/constants/alertOptions.ts
@@ -0,0 +1,82 @@
+export const ALERT_SEVERITIES = [
+  "security",
+  "critical",
+  "major",
+  "minor",
+  "warning",
+  "informational",
+  "debug",
+  "trace",
+  "indeterminate",
+  "cleared",
+  "normal",
+  "ok",
+  "unknown",
+] as const;
+
+export type AlertSeverity = (typeof ALERT_SEVERITIES)[number];
+
+
+export const ALERT_STATUSES = [
+  "open",
+  "assign",
+  "ack",
+  "closed",
+  "expired",
+  "blackout",
+  "shelved",
+  "unknown",
+] as const;
+
+export type AlertStatus = (typeof ALERT_STATUSES)[number];
+
+export const ALERT_ACTIONS = ["open",
+   "ack",
+   "unack",
+   "close",
+   "delete",
+   "shelve",
+   "unshelve",
+   "assign",
+   "note" ] as const 
+
+
+export const ALERTS_FILTERS = [
+    "status", "severity", "country", "from-date", "to-date", "resource"
+] as const
+
+export const ALERT_CATEGORIES = [
+     "data-management"
+     ,"information"
+     ,"support-requests"
+     ,"EEZ"
+     ,"beaching"
+     ,"icing"
+     ,"technical alerts"
+     ,"argo technical alerts"
+     ,"file checker"
+     ,"QC feedback"
+     ,"metadata check"
+     ,"platform"
+     ,"Cruises"
+     ,"data"
+     ,"Deployment/Retrieval"
+     ,"Data processing"
+     ,"Data access"
+     ,"Dashboard"] as const
+
+export type AlertCategory = (typeof ALERT_CATEGORIES) [number]
+
+export const ALERT_MQTT_TOPICS = ["operations-alerts"
+    , "data-management"
+    , "information"
+    , "support-requests"]
+    
+export type MqttTopic = (typeof ALERT_MQTT_TOPICS) [number]
+
+export type AlertFilters = (typeof ALERTS_FILTERS)[number]
+
+export type ActionType = (typeof ALERT_ACTIONS)[number]
+
+export type ChangeType = ActionType |  "new" | "action" | "status" | "value" | "severity" | "dismiss" | "timeout" | "expired" 
+
