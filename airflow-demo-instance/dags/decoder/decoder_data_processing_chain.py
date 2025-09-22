66ece81701c06fea3f5ddc52e0c220c8d3723e60
BRUVRY-LAGADEC <leo.brurvy.lagadec@ifremer.fr>
2025-09-22 15:51:03 +0200

    fix: :construction_worker: Airflow decoder processing chain
    First version with decoder running

--- airflow-demo-instance/dags/decoder/decoder_data_processing_chain.py
+++ airflow-demo-instance/dags/decoder/decoder_data_processing_chain.py
@@ -3,13 +3,16 @@
 from __future__ import annotations
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.models.param import Param
 from airflow.providers.singularity.operators.singularity import SingularityOperator
 
 HOST_DATA_DIRECTORY = "/opt/airflow/data"
+HOST_MATLAB_RUNTIME = "/opt/matlab/runtime"
+DEFAULT_WMO = 6902892
 
 processor_dag = DAG(
     dag_id="argo-decoder-data-processing-chain",
-    dag_display_name="#️⃣ Argo - Decoder data processing chain",
+    dag_display_name="#️⃣ Argo demonstration - Decoder data processing chain",
     default_args={
         "owner": "lbruvryl",
         "depends_on_past": False,
@@ -21,33 +24,38 @@ processor_dag = DAG(
         "retry_delay": timedelta(seconds=30),
         "retry_exponential_backoff": True,
     },
-    description="Validates a single MQTT message, then triggers the publish DAG.",
-    schedule=None,  # déclenché par le listener
+    description="Run Argo decoder Container from https://github.com/euroargodev/Coriolis-data-processing-chain-for-Argo-floats-container",
+    schedule=None,
     catchup=False,
     is_paused_upon_creation=False,
     tags=["Argo", "AMRIT", "Decoder"],
+    render_template_as_native_obj=True,
+    params={
+        "float_wmo": Param(
+            DEFAULT_WMO,
+            type="integer",
+            title="Float WMO",
+            description="WMO of Argo float with data "
+        )
+    },
 )
 
 
 decoder_data_processing_task = SingularityOperator(
     task_id="decoder_data_processing_task",
     image="docker://ghcr.io/euroargodev/coriolis-data-processing-chain-for-argo-floats-container:066a",
-    command="python3 /app/wis2_data_processing_chain/standards/validate_stac.py",
+    command="/app/entrypoint.sh /mnt/runtime rsynclog all configfile /mnt/data/config/decoder_conf.json xmlreport co041404_20250922_6902892.xml floatwmo 6902892 PROCESS_REMAINING_BUFFERS 1",
     force_pull=False,
     options=[
         "--bind",
-        "runtime-matlab-volume:/mnt/runtime:ro",
+        f"{HOST_MATLAB_RUNTIME}/R2022b:/mnt/runtime:ro",
         "--bind",
-        f"{HOST_DATA_DIRECTORY}/decArgo_demo_inputs/input:/mnt/data/rsync:rw",
+        f"{HOST_DATA_DIRECTORY}/decArgo_demo/input:/mnt/data/rsync:rw",
         "--bind",
-        f"{HOST_DATA_DIRECTORY}/decArgo_demo_inputs/config:/mnt/data/config:ro",
+        f"{HOST_DATA_DIRECTORY}/decArgo_demo/config:/mnt/data/config:ro",
         "--bind",
-        f"{HOST_DATA_DIRECTORY}/decArgo_demo_outputs:/mnt/data/output:rw",
+        f"{HOST_DATA_DIRECTORY}/decArgo_demo/outputs:/mnt/data/output:rw",
     ],
-    environment={
-        # "APPTAINER_DOCKER_USERNAME": "{{ var.value.project_registry_user }}",
-        # "APPTAINER_DOCKER_PASSWORD": "{{ var.value.project_registry_password }}",
-    },
     dag=processor_dag,
 )
 
